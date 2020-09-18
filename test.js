7b1c5eb36a4b53c25bbad82c8f6a9d2f5e441287
luis <luis.hansen@certsys.com.br>
2020-09-18 14:19:31 -0300

    test(cupom): adicionado teste de cupom que falha

--- test.js
+++ test.js
@@ -1,13 +1,29 @@
 const CarrinhoDeCompras = require('./CarrinhoDeCompras.js');
 
-describe('roda o app', function() {
+describe('roda a aplicação', function() {
 
-  it('calcula preço final com 5 reais de frete', function() {
-    var produto = {nome: "foo", preco: 10, cupom: "5REAIS"}
-    var shoppingCart = new CarrinhoDeCompras([produto]);
-    var results = shoppingCart.fecharPedido("Rua 1");
+  it('calcula preço final com 5 reais de frete e sem cupom', function() {
+
+    const produto = {nome: "foo", preco: 10, cupom: null}
+
+    const carrinho = new CarrinhoDeCompras([produto]);
+
+    const results = carrinho.fecharPedido("Rua 1");
 
     expect(results).toEqual(15);
+
+  });
+
+  it('calcula preço final com 5 reais de frete e com cupom de 5 reais', function() {
+
+    const produto = {nome: "bar", preco: 10, cupom: "5REAIS"}
+
+    const carrinho = new CarrinhoDeCompras([produto]);
+
+    const results = carrinho.fecharPedido("Rua 1");
+
+    expect(results).toEqual(10);
+
   });
 
 });
